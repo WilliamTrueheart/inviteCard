7010a1202dd274e8be204caeb4ceeacc4170b41b
Will Newton <91620451+WilliamTrueheart@users.noreply.github.com>
2022-04-30 12:43:18 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -22,7 +22,7 @@ const completeExp = () => {
     const userName = document.getElementById('firstName').value;
     const userNameFI = userName[0].toUpperCase();
     const animal = document.getElementById('animalName').value;
-    const animalFI = animal[0].toUpperCase();
+    const mainbox = document.getElementById('mainUICard');
 
     const returnText = document.getElementById('returnText')
     const radio1 = document.getElementById('radio1');
@@ -32,7 +32,8 @@ const completeExp = () => {
     polarBear.classList.remove('polarBearActive')
     polarBear2.classList.remove('polarBearActive')
     polarBear3.classList.remove('polarBearActive')
-    page3.style.display ='flex'
+    page3.style.display ='flex';
+    mainbox.style.boxShadow = '0px 0px 6px 1px white';
 
     returnText.innerHTML = 'Thanks for accepting our invite ' + userNameFI + ' - ' + animal +  '.  Woah, now that\'s a nickname with some umph behind it. Stop by for our grand opening Decemeber 1st, and let us treat you to a drink, free of charge!'
     if (radio1.checked) {
